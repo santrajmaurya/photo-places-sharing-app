cb325d800a2054b41d0a7cf61eb34f695994f942
Santraj <santrajmaurya1992@gmail.com>
2020-05-25 01:34:35 +0530

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'; 
+
+const App: React.FC = () => {
+  return (
+
+  );
+}
+
+export default App;
